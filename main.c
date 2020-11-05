24eca3865f56ae751162c44d75202b94c46b4ddc
Chris Stokes <christophermichaelstokes@gmail.com>
2020-11-05 02:38:15 +0000

    Added new tests, and reformatted to use new print function

--- main.c
+++ main.c
@@ -4,16 +4,54 @@
 
 int main(void)
 {
+    printf("######################################\n"
+           "Testing basic functionality\n"
+           "######################################\n");
+
     vector * v = init_vector();
-    for (int i = 0; i < 11; ++i)
+    for (int i = 0; i < 11; ++i) {
         vec_push_back(v, i);
-    for (int i = 0; i < v->size; ++i)
-        printf("%d\t", v->inner_array[i]);
-    putchar('\n');
-    printf("capacity: %d\n",v->max_capacity);
-    printf("size: %d\n",v->size);
+    }
+    print_vec(v);
+    printf("capacity: %lu\n",v->max_capacity);
+    printf("size: %lu\n",v->size);
     vec_shrink_to_fit(v);
-    printf("capacity: %d\n",v->max_capacity);
-    printf("size: %d\n",v->size);
+    printf("capacity: %lu\n",v->max_capacity);
+    printf("size: %lu\n",v->size);
+    free_vec(v);
+
+    printf("######################################\n"
+           "Testing arithmetic\n"
+           "######################################\n");
+    // test two equally sized vectors
+    size_t vec_len = 10;
+    vector * v1 = init_sized_vector(vec_len);
+    vector * v2 = init_sized_vector(vec_len);
+    for (int i = 0; i < vec_len; ++i) {
+	    v1->inner_array[i] = i+1;
+	    v2->inner_array[i] = (i+1)*2;
+    }
+    printf("v1:\t ");
+    print_vec(v1);
+    printf("v2:\t ");
+    print_vec(v2);
+    vector * v3 = vec_add(v1, v2); 
+    printf("v1 + v2: ");
+    print_vec(v3); free_vec(v3);
+
+    v3 = vec_multiply(v1, v2);
+    printf("v1 * v2: ");
+    print_vec(v3); free_vec(v3);
+    
+    v3 = vec_subtract(v1, v2);
+    printf("v1 - v2: ");
+    print_vec(v3); free_vec(v3);
+
+    v3 = vec_divide(v2, v1);
+    printf("v2 / v1: ");
+    print_vec(v3); free_vec(v3);
+
+    free_vec(v1); free_vec(v2);
+    
     return 0;
 }
